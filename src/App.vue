3f5287a02de3110e12019a9f420c460a5278e37c
Kehinde Balogun <balogunkehinde3@gmail.com>
2023-11-21 09:47:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+ import Block from './components/game-Block.vue'
+ import Result from './components/Result.vue';
+</script>
+
+<template>
+   <div class="main">
+        <h1>Reaction Timer</h1>
+        <button @click="start" :disabled="isPlaying">play</button>
+   </div>
+   <Block :delay="delay" v-if="isPlaying" @end="endGame"/>
+   <Result :score="score" v-if="score"/>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data (){
+     return {
+        isPlaying: false,
+        delay: null,
+        score: null,
+    }
+    },
+    methods: {
+        start() {
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlaying = true
+        this.score= null
+        },
+
+        endGame(reaction){
+          this.score = reaction
+          this.isPlaying = false
+        }
+    }
+  }
+</script>
+
+
+<style>
+     .main {
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        margin: 100px auto;
+     }
+</style>
---
+++ src/components/game-Result.vue
@@ -0,0 +1,30 @@
+<template>
+    <h3>Your reaction time is {{ score }}</h3>
+    <P v-if="rank"> {{ rank }} </P>
+</template>
+
+<script>
+  export default {
+       props: ['score'],
+       data(){
+        return {
+            rank: null
+        }
+       },
+       mounted(){
+        if(this.score < 250){
+            this.rank = "Ninja Reflexes"
+        } else if (this.score < 400){
+            this.rank = "Rapid Reflexes"
+        } else {
+            this.rank = "Snail Reflexes"
+        }
+       }
+  }
+</script>
+
+<style>
+    h3, p{
+        text-align: center;
+    }
+</style>
